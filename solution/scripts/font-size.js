2ab3075d7952f6067063c012ec570758a8713e28
Roberto Cuervo <rcuervoa@hsr.ch>
2017-05-10 10:13:58 +0200

    Added slider implementation
    Signed-off-by: Roberto Cuervo <rcuervoa@hsr.ch>

--- solution/scripts/font-size.js
+++ solution/scripts/font-size.js
@@ -1,8 +1,8 @@
 /**
  * Created by roberto on 03.05.17.
  */
-$('input').on('change', function () {
+$("input").on('change', function () {
     var v = $(this).val();
-    $('div').css('font-size', v + 'px')
-    $('span').html(v);
+    $('html').css('font-size', v + 'px')
+    $('#font-size').html(v);
 });

f63b6fb7c54134827b48c65e8eac0bdae727b4f2
Roberto Cuervo <rcuervoa@hsr.ch>
2017-05-17 12:46:35 +0200

    Modifications after validation tool
    Signed-off-by: Roberto Cuervo <rcuervoa@hsr.ch>

--- solution/calculator/scripts/calculator.js
+++ solution/calculator/scripts/calculator.js
@@ -1,127 +1,129 @@
 /**
  * core
  */
-var calculatorState = {
-    operand1: undefined,
-    operand2: undefined,
-    operator: undefined
-};
+(function () {
+    "use strict";
+    var calculatorState = {
+        operand1: undefined,
+        operand2: undefined,
+        operator: undefined
+    };
 
-function doCalculation() {
-    if (calculatorState.operand1 === undefined || calculatorState.operand2 === undefined) {
-        return undefined;
-    }
-    switch (calculatorState.operator) {
-        case '+':
-            return calculatorState.operand1 + calculatorState.operand2;
-            break;
-        case '-':
-            return calculatorState.operand1 - calculatorState.operand2;
-            break;
-        case '/':
-            return (calculatorState.operand2 === 0) ? undefined : (calculatorState.operand1 / calculatorState.operand2);
-            break;
-        case '*':
-            return calculatorState.operand1 * calculatorState.operand2;
-            break;
-        default:
+    function doCalculation() {
+        if (calculatorState.operand1 === undefined || calculatorState.operand2 === undefined) {
             return undefined;
+        }
+        switch (calculatorState.operator) {
+            case '+':
+                return calculatorState.operand1 + calculatorState.operand2;
+            case '-':
+                return calculatorState.operand1 - calculatorState.operand2;
+            case '/':
+                return (calculatorState.operand2 === 0) ? undefined : (calculatorState.operand1 / calculatorState.operand2);
+            case '*':
+                return calculatorState.operand1 * calculatorState.operand2;
+            default:
+                return undefined;
+        }
     }
-}
 
-function defineOperator(operator) {
-    calculatorState.operator = operator;
-    return calculatorState;
-}
+    function defineOperator(operator) {
+        calculatorState.operator = operator;
+        return calculatorState;
+    }
 
-function calculate() {
-    if (calculatorState.operand2 != undefined) {
-        calculatorState.operand1 = doCalculation();
+    function calculate() {
+        if (calculatorState.operand2 !== undefined) {
+            calculatorState.operand1 = doCalculation();
+            calculatorState.operand2 = undefined;
+        }
+        return calculatorState;
+    }
+
+    function calculateNewNumberWithDigit(number, digit) {
+        var currentValue = (number === undefined) ? 0 : number;
+        return (currentValue * 10) + parseInt(digit);
+    }
+
+    function updateCurrentInputValue(digit) {
+        if (calculatorState.operator === undefined) {
+            calculatorState.operand1 = calculateNewNumberWithDigit(calculatorState.operand1, digit);
+        } else {
+            calculatorState.operand2 = calculateNewNumberWithDigit(calculatorState.operand2, digit);
+        }
+        return calculatorState;
+    }
+
+
+    function clear() {
+        calculatorState.operand1 = undefined;
         calculatorState.operand2 = undefined;
+        calculatorState.operator = undefined;
     }
-    return calculatorState;
-}
 
-function updateCurrentInputValue(digit) {
-    if (calculatorState.operator === undefined) {
-        calculatorState.operand1 = calculateNewNumberWithDigit(calculatorState.operand1, digit);
-    } else {
-        calculatorState.operand2 = calculateNewNumberWithDigit(calculatorState.operand2, digit);
+    /**
+     * UI
+     */
+
+    function writeOutput(output) {
+        $("#output").html(output);
     }
-    return calculatorState;
-}
 
-function calculateNewNumberWithDigit(number, digit) {
-    var currentValue = (number === undefined) ? 0 : number;
-    return (currentValue * 10) + parseInt(digit);
-}
-
-function clear() {
-    calculatorState.operand1 = undefined;
-    calculatorState.operand2 = undefined;
-    calculatorState.operator = undefined;
-}
-
-/**
- * UI
- */
-$(function () {
-    writeOutput("Welcome");
-    initializeClickHandlers();
-});
-
-function initializeClickHandlers() {
-    $("button").click(welcomeClickHandler);
-    $(".number").click(numberClickHandler);
-    $(".operator").click(operatorClickHandler);
-    $("#key-\\=").click(equalsClickHandler);
-    $("#key-c").click(clearClickHandler);
-}
-
-function welcomeClickHandler() {
-    writeOutput("");
-    $("button").unbind("click", welcomeClickHandler)
-}
-
-function numberClickHandler() {
-    var state = updateCurrentInputValue(this.value);
-    if (state.operator === undefined) {
-        writeInput(state.operand1);
-    } else {
-        writeInput(state.operand2);
+    function writeInput(input) {
+        $("#input").html(input);
     }
-}
 
-function operatorClickHandler() {
-    var state = defineOperator(this.value);
-    if (state.operand2 === undefined) {
-        writeInput("");
+    function welcomeClickHandler() {
+        writeOutput("");
+        $("button").unbind("click", welcomeClickHandler);
     }
-    writeOutput(((state.operand1 === undefined) ? "" : (state.operand1 + " ")) + state.operator);
-}
 
-function equalsClickHandler() {
-    var state = calculate();
-    if (state.operand1 === undefined) {
-        writeOutput("Invalid calculation");
+    function numberClickHandler(event) {
+        var state = updateCurrentInputValue(event.target.value);
+        if (state.operator === undefined) {
+            writeInput(state.operand1);
+        } else {
+            writeInput(state.operand2);
+        }
+    }
+
+    function operatorClickHandler(event) {
+        var state = defineOperator(event.target.value);
+        if (state.operand2 === undefined) {
+            writeInput("");
+        }
+        writeOutput(((state.operand1 === undefined) ? "" : (state.operand1 + " ")) + state.operator);
+    }
+
+    function equalsClickHandler() {
+        var state = calculate();
+        if (state.operand1 === undefined) {
+            writeOutput("Invalid calculation");
+            clear();
+        } else {
+            writeInput(state.operand1);
+            writeOutput("");
+        }
+    }
+
+    function clearClickHandler() {
         clear();
-    } else {
-        writeInput(state.operand1);
+        writeInput("");
         writeOutput("");
     }
-}
 
-function clearClickHandler() {
-    clear();
-    writeInput("");
-    writeOutput("");
-}
 
-function writeOutput(output) {
-    $("#output").html(output);
-}
+    function initializeClickHandlers() {
+        $("button").click(welcomeClickHandler);
+        $(".number").click(numberClickHandler);
+        $(".operator").click(operatorClickHandler);
+        $("#key-\\=").click(equalsClickHandler);
+        $("#key-c").click(clearClickHandler);
+    }
 
-function writeInput(input) {
-    $("#input").html(input);
-}
+    $(function () {
+        writeOutput("Welcome");
+        initializeClickHandlers();
+    });
 
+})();
--- solution/scripts/font-size.js
+++ solution/scripts/font-size.js
@@ -1,8 +1,11 @@
 /**
  * Created by roberto on 03.05.17.
  */
-$("input").on("change", function () {
-    var v = $(this).val();
-    $("html").css("font-size", v + "px")
-    $("#font-size").html(v);
-});
+(function () {
+    "use strict";
+    $("input").on("change", function () {
+        var v = $(this).val();
+        $("html").css("font-size", v + "px");
+        $("#font-size").html(v);
+    });
+})();
